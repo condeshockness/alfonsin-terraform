73604ec230653063745debb404d4212fcb0a1c92
Alfonsin <ramonsilas.dev@gmail.com>
2025-05-10 18:28:06 -0400

    ok

--- modules/lxc/servers/main.tf
+++ modules/lxc/servers/main.tf
@@ -21,7 +21,6 @@ resource "proxmox_lxc" "lxc" {
   memory          = var.memory
   swap            = var.memory_swap
   password        = var.user_password
-  #ssh_public_keys = (var.user_ssh_key_public != null ? file("${var.user_ssh_key_public}") : null)
 
   ssh_public_keys = <<-EOT
     ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBgXYoz7pCRQ5LcwHRn0andCTvN+lpfmj2mUOD7C2wqg alfonsin.nuvem@gmail.com
